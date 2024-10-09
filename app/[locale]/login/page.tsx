1b68bf6ef628a03f0e5ca844683cbaabf3377045
GrupoGransolar <40598104+GrupoGransolar@users.noreply.github.com>
2024-10-09 10:28:22 +0200

    Update page.tsx

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,52 +1,40 @@
-"use client"
+const handleSSOLogin = async () => {
+  const supabase = createClient()
+  try {
+    const { data, error } = await supabase.auth.signInWithSSO({
+      domain: 'gransolar.com'
+    })
 
-import { useRouter } from 'next/navigation'
-import { createClient } from '@/lib/supabase/server'
-import { useState } from 'react'
+    if (error) {
+      console.error("Error SSO:", error.message)
+      return
+    }
 
-export default function Login() {
-  const [error, setError] = useState(null)
-  const router = useRouter()
+    // Aquí deberías obtener el ID del workspace una vez la autenticación sea exitosa
+    const session = await supabase.auth.getSession()
 
-  const handleSSOLogin = async (e) => {
-    e.preventDefault() // Evita que el formulario recargue la página
+    if (session?.data?.session) {
+      const { user } = session.data.session
 
-    const supabase = createClient()
+      // Buscar el workspace correspondiente al usuario autenticado
+      const { data: homeWorkspace, error: homeWorkspaceError } = await supabase
+        .from("workspaces")
+        .select("*")
+        .eq("user_id", user.id)
+        .eq("is_home", true)
+        .single()
 
-    try {
-      const { data, error } = await supabase.auth.signInWithSSO({
-        domain: 'gransolar.com' // Dominio autorizado configurado en Supabase SAML
-      })
-
-      if (error) {
-        setError(error.message) // Muestra el mensaje de error si hay uno
-        return
+      if (homeWorkspaceError || !homeWorkspace) {
+        console.error("Error obteniendo el workspace:", homeWorkspaceError?.message)
+        throw new Error(homeWorkspaceError?.message || "No se pudo obtener el workspace.")
       }
 
-      // Si el inicio de sesión SSO es exitoso, redirigir al dashboard
-      router.push("/dashboard")
-    } catch (err) {
-      setError("An unexpected error occurred")
-      console.error("SSO Login Error:", err)
+      // Redirigir al workspace correcto
+      window.location.href = `/${homeWorkspace.id}/chat`
+    } else {
+      console.error("Error obteniendo la sesión.")
     }
+  } catch (err) {
+    console.error("SSO Login Error:", err)
   }
-
-  return (
-    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
-      <div className="text-center mb-4">
-        <h1 className="text-2xl font-bold">Login with SSO</h1>
-      </div>
-
-      <form onSubmit={handleSSOLogin}>
-        <button
-          type="submit"
-          className="mb-6 w-full rounded-md bg-blue-700 px-4 py-2 text-white"
-        >
-          Sign in with SSO
-        </button>
-      </form>
-
-      {error && <p className="text-red-500 mt-4">{error}</p>}
-    </div>
-  )
 }
