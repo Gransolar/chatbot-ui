791a2f69a6c3462c7676fac1b9dd4aa9f98e1b0e
Victor Saldarriaga Lau <vjsaldarriga.ext@gransolar.com>
2024-10-10 16:20:57 +0200

    added onAuthStateChange events

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -51,6 +51,30 @@ export default async function Login({
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
+    // Register this immediately after calling createClient!
+    // Because signInWithOAuth causes a redirect, you need to fetch the
+    // provider tokens from the callback.
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (session && session.provider_token) {
+        window.localStorage.setItem(
+          "oauth_provider_token",
+          session.provider_token
+        )
+      }
+
+      if (session && session.provider_refresh_token) {
+        window.localStorage.setItem(
+          "oauth_provider_refresh_token",
+          session.provider_refresh_token
+        )
+      }
+
+      if (event === "SIGNED_OUT") {
+        window.localStorage.removeItem("oauth_provider_token")
+        window.localStorage.removeItem("oauth_provider_refresh_token")
+      }
+    })
+
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "azure",
       options: {
