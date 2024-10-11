052439a906d8d84f8f23e8e0d1d33ae6f1fec1c9
Victor Saldarriaga Lau <vjsaldarriga.ext@gransolar.com>
2024-10-11 09:26:13 +0200

    restore middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -57,20 +57,5 @@ export const createClient = (request: NextRequest) => {
     }
   )
 
-  const {
-    data: { user }
-  } = await supabase.auth.getUser()
-
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone()
-    url.pathname = "/login"
-    return NextResponse.redirect(url)
-  }
-
   return { supabase, response }
 }
