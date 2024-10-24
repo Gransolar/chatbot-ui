1bd5b6eb3831d5a615c4c5b7c33b3bbf12d773db
jgilgarciapvh <jgilgarcia@pvhardware.com>
2024-10-24 11:12:06 +0200

    FIX(azureRoute): fixed max_tokens param

--- app/api/chat/azure/route.ts
+++ app/api/chat/azure/route.ts
@@ -58,7 +58,8 @@ export async function POST(request: Request) {
       model: DEPLOYMENT_ID as ChatCompletionCreateParamsBase["model"],
       messages: messages as ChatCompletionCreateParamsBase["messages"],
       temperature: chatSettings.temperature,
-      max_tokens: chatSettings.model === "gpt-4-vision-preview" ? 4096 : null, // TODO: Fix
+      max_tokens: null, // TODO: Fix
+      // max_tokens: chatSettings.model === "gpt-4-vision-preview" ? 4096 : null, // TODO: Fix
       stream: true
     })
 
